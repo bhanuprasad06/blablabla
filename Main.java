6f176588ff522b2f8a138e9f54a0e75abf7956c2
Hemanth097 <64942691+Hemanth097@users.noreply.github.com>
2022-05-24 12:16:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,39 +1,65 @@
-import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
 
-public class Main{
-
-//Main execution
-
-
-
-public static void main(String[] args){
-       Support s = new Support();
-       s.welcome();
-       s.input();
-       s.calculation();
-  
-    }
-}
-
-class Support{
-   
-//Support functions
-     void welcome(){
-        System.out.println("hello");
-        System.out.println("welcome to solve the calculator game");
+        int[] keys = new int[]{10,-2,-3,0};
+        int noMoves = 5;
+        int goal = 15;
         
-    }
+        int inputs = keys.length;
+        int n = noMoves;
+        int[] moves = new int[noMoves+1];
 
-void input(){
-        Scanner input = new Scanner(System.in);
+        int goalCal;
+        int checker = 0;
+        int possSol = 0;
+        int combi = 0;
+
         
-        System.out.println("enter number of moves");
-      //  moves = input.nextInt();
-        System.out.println("enter number of keys");        System.out.println("enter the goal");
+        for(int i = 0; i < n; i++){
+            moves[i] = 0;
+        }
+
+        if (inputs == 1) {
+          for(int i = n; i > 0; i--){
+                System.out.print(keys[moves[i]] + " ");
+                checker++;
+            }
+        }else{
+          
+           while(moves[0] == 0){
+             goalCal = 0;
+             for(int i = n; i > 0; i--){
+                  goalCal += keys[moves[i]];
+                  checker++;
+
+              }
+              combi++;
+              if (goalCal == goal) {
+                
+                for(int i = n; i > 0; i--){
+                    System.out.print(keys[moves[i]] + " ");
+                 }
+                System.out.println("");
+                possSol++;
+                //System.out.println(keys[moves[n]] + " " + keys[moves[n-1]] + " " + keys[moves[n-2]]);
+                
+              }
+              moves = incfun(moves, n, inputs);
+          }
+      }
+        System.out.println("Total calculations done are " + checker);
+        System.out.println("Total Combinations done are " + combi);
+        System.out.println("Total Possible Solutions are " + possSol);
     }
-   
     
-    void calculation(){
+    static int[] incfun(int[] moves, int n, int inputs){
+        moves[n]++;
         
+        if(moves[n] == inputs){
+            moves[n] = 0;
+            moves = incfun(moves, n-1, inputs);
+            
+        }
+        return moves;
     }
 }
