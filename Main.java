3bcc673ee2fac00ada2e65570b394392caf714b2
Hemanth097 <64942691+Hemanth097@users.noreply.github.com>
2022-05-25 11:46:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
+import java.util.Scanner; 
+
 class Main {
     public static void main(String[] args) {
 
-        int[] keys = new int[]{10,-2,-3,0};
+        String[] keys = new String[6];
         int noMoves = 5;
         int goal = 15;
         
-        int inputs = keys.length;
+        int inputs = 0;
         int n = noMoves;
         int[] moves = new int[noMoves+1];
 
@@ -14,6 +16,18 @@ class Main {
         int possSol = 0;
         int combi = 0;
 
+        Support sup = new Support();
+        Functions fun = new Functions();
+        Scanner inp = new Scanner(System.in);
+
+        for(int i = 0; i <6; i++){
+          keys[i] = inp.nextLine();
+          if(keys[i] == "0"){
+            break;
+          }else{
+            inputs++;
+          }
+        }
         
         for(int i = 0; i < n; i++){
             moves[i] = 0;
@@ -24,12 +38,12 @@ class Main {
                 System.out.print(keys[moves[i]] + " ");
                 checker++;
             }
-        }else{
+        }else{    
           
            while(moves[0] == 0){
              goalCal = 0;
              for(int i = n; i > 0; i--){
-                  goalCal += keys[moves[i]];
+                  goalCal = fun.addition(goalCal, parseInt(keys[moves[i]]));
                   checker++;
 
               }
@@ -44,7 +58,7 @@ class Main {
                 //System.out.println(keys[moves[n]] + " " + keys[moves[n-1]] + " " + keys[moves[n-2]]);
                 
               }
-              moves = incfun(moves, n, inputs);
+              moves = sup.incfun(moves, n, inputs);
           }
       }
         System.out.println("Total calculations done are " + checker);
@@ -52,7 +66,10 @@ class Main {
         System.out.println("Total Possible Solutions are " + possSol);
     }
     
-    static int[] incfun(int[] moves, int n, int inputs){
+}
+
+class Support{
+  int[] incfun(int[] moves, int n, int inputs){
         moves[n]++;
         
         if(moves[n] == inputs){
@@ -62,4 +79,16 @@ class Main {
         }
         return moves;
     }
+}
+
+class Functions{
+  int addition(int goalCal, int x){
+    goalCal +=x;
+    return goalCal;
+  }
+
+  int multiplication(int goalCal, int x){
+    goalCal *= x;
+    return goalCal;
+  }
 }
